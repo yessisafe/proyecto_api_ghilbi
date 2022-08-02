3a76eab9d506c8d1bc0465f55ecaf6d4de66edc6
Yessica <yessisafe@gmail.com>
2022-08-02 00:11:53 -0500

    agregando api e informacion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-
+//funcion fetch
 fetch('https://ghibliapi.herokuapp.com/films')
   .then((response)=>response.json())
   .then((data)=>{
-    console.log(data);
+    dibujar(data);
   })
 
 
   //const Dibujar
-  const dibujar= () => {
+  const dibujar= (peliculas) => {
+    let div =document.createElement("div");
+    div.classList.add("column", "is-3");
+    document.querySelector("#movies").innerHTML="";
+
+    peliculas.forEach((pelicula) => {
+      //Cada pelicula
+      let div =document.createElement("div");
+      div.classList.add("column", "is-3");
+
+      div.innerHTML +=`<div class="card">
+                      <div class="card-image">
+                        <figure class="image is-4by5">
+                          <img src="${pelicula.image}" alt="Placeholder image"/>
+                        </figure>
+                      </div>
+                          <div class="card-content">
+                              <p class="title is-4">${pelicula.title}</p>
+                              
+                              <h5 class="subtitle is-6">${pelicula.release_date}</h5>
+                          </div>
+                      </div>`
+
+       document.querySelector("#movies").append(div);
+
+    });
+
+    
+
 
   }
